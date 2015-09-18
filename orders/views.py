e86523ad9aefa4886aa5a46d1bc34cf39e5285d3
Amoki <hugo.duroux@gmail.com>
2015-09-18 16:33:22 +0200

    Merge same pizza_order, thanks to @Crocmagnon

--- orders/views.py
+++ orders/views.py
@@ -4,9 +4,7 @@ from orders.models import Order, PizzaOrder, Debil
 from orders.forms import PizzaOrderForm
 from orders.signals import INVITED_PREFIX
 
-
 import random
-
 
 WALKERS = 2
 
@@ -17,7 +15,7 @@ def home(request):
 
 
 def order(request):
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         form = PizzaOrderForm(request.POST)
         if form.is_valid():
             PizzaOrder(
@@ -42,20 +40,33 @@ def orders_history(request):
     to_return = []
 
     for order in last_orders:
-        pizzaOrders = order.pizzaorder_set.all()
+        pizza_orders = order.pizzaorder_set.all()
 
+        treated_pizzas_orders = []
+        formatted_order = []
 
+        for pizza_order in pizza_orders:
+            if pizza_order not in treated_pizzas_orders:
+                treated_pizzas_orders.append(pizza_order)
+                same_pizzas = []
+                debil_list = [pizza_order.debil.name]
+                for other_pizza_order in pizza_orders:
+                    if other_pizza_order not in treated_pizzas_orders and other_pizza_order.get_verbose_description() == pizza_order.get_verbose_description():
+                        treated_pizzas_orders.append(other_pizza_order)
+                        same_pizzas.append(other_pizza_order)
+                        debil_list.append(other_pizza_order.debil.name)
+                quantity = 1 + len(same_pizzas)
+                formatted_order.append({
+                    "quantity": quantity,
+                    "pizza": pizza_order,
+                    "price": pizza_order.get_price() * quantity,
+                    "debil_list": debil_list
+                })
 
+        to_return.append({"content": formatted_order,
+                          "info": order})
 
-
-    # @output : list of last_orders that contain a list of dict
-    # {
-    #   number: int,
-    #   pizza_with_crust: pizzaOrder.get_verbose_description()
-    # }
-
-    # TODO: update view
-    return render(request, 'orders-history.html', {"last_orders": last_orders})
+    return render(request, 'orders-history.html', {"last_orders": to_return})
 
 
 def who_work_today(request):
@@ -69,8 +80,12 @@ def who_work_today(request):
         index = random.randrange(len(eligible_walkers))
         walkers.append(eligible_walkers.pop(index))
 
-    eligible_commanders = list(Debil.objects.filter(pizzaorder__order=last_order).exclude(name__startswith=INVITED_PREFIX).exclude(name__in=[walker.name for walker in walkers]))
-
+    eligible_commanders = list(
+        Debil.objects
+        .filter(pizzaorder__order=last_order)
+        .exclude(name__startswith=INVITED_PREFIX)
+        .exclude(name__in=[walker.name for walker in walkers])
+    )
     # Pick the commander
     index = random.randrange(len(eligible_commanders))
     commander = eligible_commanders.pop(index)
